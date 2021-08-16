1f4483d79c539a509a782f75ec832af7913a7bcb
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-08-16 23:45:49 +0300

    added the token part and the database part

--- server.js
+++ server.js
@@ -2,23 +2,87 @@
 
 require('dotenv').config();
 const express = require('express');
+const app = express();
+const router = express.Router();
+
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
-
-const app = express();
-app.use(cors());
+const mongoose = require('mongoose');
+const { request, response } = require('express');
 
 const PORT = process.env.PORT || 3001;
+const { Schema } = mongoose;
 
-app.get('/test', (request, response) => {
+app.use(cors());
+app.use('/', router); // base
 
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+const client = jwksClient({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+});
 
-})
+
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+};
+
+
+router.route('/authorize').get( (req, res) => {
+  console.log(req.headers.authorization );
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, getKey, {}, (err, user) => {
+    if(err){
+      res.send('invalid token');
+    }
+    res.send(token);
+  });
+});
+
+
+mongoose.connect('mongodb://localhost:27017/books', { useNewUrlParser: true, seUnifiedTopology: true });
+
+// defining the schema
+const bookSchema = new Schema({
+  title: String,
+  description: String,
+  status: String,
+  email: String
+});
+
+// defining the book model
+const Book = mongoose.model('books', bookSchema);
+
+const connection = mongoose.connection;
+
+// check the connection to the database 
+connection.once('open', () => console.log('MongoDB connection established successfully'));
+
+const seedBook = ({title, description, status, email}) => {
+  const nBook = new Book({
+    title: title,
+    description: description,
+    status: status,
+    email: email
+  });
+  nBook.save();
+}
+
+// store the book
+router.route('/saveBook').get((request, response)=> {
+  let bookReq = request.query;
+  seedBook(bookReq);
+  response.send(`The book is saved successfully.`)
+});
+
+// get the book
+router.route('/books').get((request, response) => {
+  Book.find({}, (err, result) => {
+    if (err) response.send(err)
+    else response.send(result);
+  });
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
